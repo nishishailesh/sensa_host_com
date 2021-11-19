b4ba1c50a378e0423061b22e32932ff32283326d
Your Name <you@example.com>
2021-11-19 10:23:39 +0530

    Started tarted

---
+++ misc/untitled.py
@@ -0,0 +1,44 @@
+all record: 
+	{
+		b'1297254': {
+						b'Date': b'time: 18-11-2021 11:46:59', 
+						b'': '', b'Patient ID : 1297254': '', 
+						b'Name       :': '', 
+						b'Gender     :': '', 
+						b'Age        :': '', 
+						b'Department :': '', 
+						b'Hospital Name  :': '', 
+						b'----------------------': '', 
+						b'FiO2       : 0.21': '', 
+						b'Pat. Temp  : 37.0': '', 
+						b'TRef       : 26.250': '', 
+						b'BLOOD GAS': '', 
+						b'pCO2': b'24.86 mmHg', 
+						b'pO2': b'56.84 mmHg', 
+						b'HEMATOCRIT': '', 
+						b'Hct': b'29.67 %', 
+						b'SO2': b'92.84 %', 
+						b'Hb': b'9.89 g/dL', 
+						b'ELECTROLYTES': '', 
+						b'Na': b'146.8 mmol/L', 
+						b'K': b'3.26 mmol/L', b'iCa': b'0.93 mmol/L', b'Cl': b'115.6 mmol/L', b'Li': b'NA  NA', b'nCa': b'1.00 mmol/L', b'TCa': b'1.99 mmol/L', b'METABOLITES': '', b'GLU': b'104.9 mg/dL', b'LAC': b'1.60 mmol/L', b'CALCULATED PARAMETERS': '', b'HCO3': b'20.99 mmol/L', b'TCO2': b'21.74 mmol/L', b'SBC': b'24.33 mmol/L', b'O2Ct': b'12.94 Vol%', b'pO2%': b'7.48 %', b'BE': b'-1.36 mmol/L', b'BE-B': b'-0.13 mmol/L', b'BE-ECF': b'-1.89 mmol/L', b'Anion Gap': '', b'AG-Na': b'10.24 mmol/L', b'AG-K': b'13.50 mmol/L', b'Alveolar oxygen': '', b'A': b'119.9 mmHg', b'AaDO2': b'63.12 mmHg', b'a/A': b'47.38 %', b'\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0': ''}}
+
+
+
+{b'1297254': {
+				b'Date': b'time: 18-11-2021 11:46:59', 
+				b'': '', b'Patient ID : 1297254': '', 
+				b'Name       :': '', b'Gender     :': '', b'Age        :': '', b'Department :': '', b'Hospital Name  :': '', 
+				b'----------------------': '', b'FiO2       : 0.21': '', b'Pat. Temp  : 37.0': '', b'TRef       : 26.250': '', 
+				b'BLOOD GAS': '', 
+				b'pH': b'7.531', 
+				b'pCO2': b'24.86 mmHg', 
+				b'pO2': b'56.84 mmHg', 
+				b'HEMATOCRIT': '', 
+				b'Hct': b'29.67 %', 
+				b'SO2': b'92.84 %', 
+				b'Hb': b'9.89 g/dL', 
+				b'ELECTROLYTES': '', 
+				b'Na': b'146.8 mmol/L', 
+				b'K': b'3.26 mmol/L', 
+				b'iCa': b'0.93 mmol/L', b'Cl': b'115.6 mmol/L', b'Li': b'NA  NA', b'nCa': b'1.00 mmol/L', b'TCa': b'1.99 mmol/L', b'METABOLITES': '', b'GLU': b'104.9 mg/dL', b'LAC': b'1.60 mmol/L', b'CALCULATED PARAMETERS': '', b'HCO3': b'20.99 mmol/L', b'TCO2': b'21.74 mmol/L', b'SBC': b'24.33 mmol/L', b'O2Ct': b'12.94 Vol%', b'pO2%': b'7.48 %', b'BE': b'-1.36 mmol/L', b'BE-B': b'-0.13 mmol/L', b'BE-ECF': b'-1.89 mmol/L', b'Anion Gap': '', b'AG-Na': b'10.24 mmol/L', b'AG-K': b'13.50 mmol/L', b'Alveolar oxygen': '', b'A': b'119.9 mmHg', b'AaDO2': b'63.12 mmHg', b'a/A': b'47.38 %', b'\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0\xc2\xb0': ''}}
